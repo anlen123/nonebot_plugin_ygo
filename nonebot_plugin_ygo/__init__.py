ddee93035ad120c7fc462a078d07ee31f8372b6c
anlen123 <1761512493@qq.com>
2022-03-15 12:57:12 +0800

    修改浏览器头为nonebot-plugin-ygo

--- nonebot_plugin_ygo/__init__.py
+++ nonebot_plugin_ygo/__init__.py
@@ -30,19 +30,8 @@ async def ygoMethod(bot: Bot, event: Event):
 async def main(key: str):
     url = f"https://ygocdb.com/?search={key}"
     headers = {
-        'authority': 'ygocdb.com',
-        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"macOS"',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'sec-fetch-site': 'same-origin',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-user': '?1',
-        'sec-fetch-dest': 'document',
+        'user-agent': 'nonebot-plugin-ygo',
         'referer': 'https://ygocdb.com/',
-        'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8'
     }
     imgs = []
     async with aiohttp.ClientSession() as session:
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nonebot_plugin_ygo',
-    version="1.0.0",
+    version="1.0.1",
     description=(
         'nonebot的游戏王卡查插件'
     ),
